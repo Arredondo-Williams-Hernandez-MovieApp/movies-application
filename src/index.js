d184382faa140b5a3193965d84c05bae577497eb
George Williams <george412williams@gmail.com>
2020-05-28 11:36:57 -0500

    requirement 1 added indexjs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
+const $ = require('jquery');
+
 /**
  * es6 modules and imports
  */
 import sayHello from './hello';
 sayHello('World');
 
+
+
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
+
+//First requirement
+$(document).ready(function() {
+  alert('loading...');
+
+});
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
