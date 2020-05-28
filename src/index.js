57c7252ad633af229f3b93efd6ee2428ce4b01b8
Jay Arredondo <jearredondo13@gmail.com>
2020-05-28 17:00:13 -0500

    tried a different approach..

--- src/index.js
+++ src/index.js
@@ -19,51 +19,55 @@ $(document).ready(function() {
   //while text input val === 0, run 'loading...'
   //on input populate html, using live search, keyup event
 //
-// $('button').click(function() {
-//   $('h1').html('Loading...');
-//   const moviePost = {title: $('#movie-title').val(), rating: $('#movie-rating').val()};
-//   const url = '/api/movies';
-//   const options = {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(moviePost),
-//   };
-
-  $.ajax('/api/movies').done(function (data) {
-    $('#loading').text(`Here are all the movies:`);
-    data.forEach(({title, rating}) => {
-      $('.container').append(`<div class="movies">${title} - rating: ${rating}</div>`)
+   getMovies().then((movies) => {
+    $('h1').html('Here are all the movies:');
+    var movieList = '';
+    movies.forEach(({title, rating, id}) => {
+      movieList += $('p').append(`id#${id} - ${title} - rating: ${rating} `);
     });
-
-  $('button').click(function (e) {
-    $('h1').html('Loading...');
-    e.preventDefault();
-    $('.container').append(`<div class="movies">${$('#movie-title').val()} - rating: ${$('#movie-rating').val()} </div>`)
-    setTimeout(function () {
-      $('h1').html('Here are your movies ')
-    },2000);
-    $.post('/api/movies', {
-      "title": $('#movie-title').val(),
-      "rating": $('#movie-rating').val()
-    });
+  }).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
   });
-//   fetch(url,options).then((movies) => {
-//     $('h1').html('Retrieving movies');
-//     movies.forEach(({title, rating, id}) =>{
-//       $('p').append(`id#${id} - ${title} - rating: ${rating} `);
-//     });
+
+
+$('button').click(function() {
+  $('h1').html('Loading...');
+      $.post('/api/movies', {
+        "title": $('#movie-title').val(),
+        "rating": $('#movie-rating').val()
+      });
+      addMovie().then(getMovies().then((movies) => {
+          $('h1').html('Here are all the movies:');
+          movies.forEach(({title, rating, id}) => {
+              $('p').html(movieList);
+          });
+      }).catch((error) => {
+          alert('Oh no! Something went wrong.\nCheck the console for details.');
+          console.log(error);
+      }));
+});
+  // $.ajax('/api/movies').done(function (data) {
+  //   $('#loading').text(`Here are all the movies:`);
+  //   data.forEach(({title, rating}) => {
+  //     $('.container').append(`<div class="movies">${title} - rating: ${rating}</div>`)
+  //   });
+  //
+  // $('button').click(function (e) {
+  //   $('h1').html('Loading...');
+  //   e.preventDefault();
+  //   $('.container').append(`<div class="movies">${$('#movie-title').val()} - rating: ${$('#movie-rating').val()} </div>`)
+  //   setTimeout(function () {
+  //     $('h1').html('Here are your movies ')
+  //   },2000);
+  //   $.post('/api/movies', {
+  //     "title": $('#movie-title').val(),
+  //     "rating": $('#movie-rating').val()
+  //   });
+  // });
 //   })
 //   })
 // });
-//   getMovies().then((movies) => {
-//     $('h1').html('Here are all the movies:');
-//     movies.forEach(({title, rating, id}) => {
-//       $('p').append(`id#${id} - ${title} - rating: ${rating} `);
-    });
-//   }).catch((error) => {
-//     alert('Oh no! Something went wrong.\nCheck the console for details.');
-//     console.log(error);
+
 //
 });
