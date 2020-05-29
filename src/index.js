a36f590d83c69ed392fb5a5f0d7e86d6d1dba17a
Jay Arredondo <jearredondo13@gmail.com>
2020-05-29 17:18:41 -0500

    stuck on having the edit button populate text fields

--- src/index.js
+++ src/index.js
@@ -19,7 +19,8 @@ $(document).ready(function() {
     var movieList = '';
     $('h1').html('Here are all the movies:');
     movies.forEach(({title, rating}) => {
-      movieList += $('.movies').append(`<div class="movieObject">${title} - rating: ${rating}</div><button>DELETE</button>`);
+      movieList += $('.movies').append(`<div class="movieObject">${title} - rating: ${rating}
+       </div><div class="edit-fields"><button class="delete">DELETE</button><button class="edit-button">EDIT</button></div>`);
         $('#edit-movie').append(`<option>${title} - rating: ${rating}</option>`)});
   }).catch((error) => {
     alert('Oh no! Something went wrong.\nCheck the console for details.');
@@ -27,12 +28,18 @@ $(document).ready(function() {
   });
 
 //edit
-
+    // edit movie functionality coming soon!
+    $('.edit-button').click(function() {
+        // $('.edit-fields').append(`<form action=""><label for="edit-title">Change Title: </label>
+        //  <input type="text" id="edit-title" name="edit-title">
+        //  <label for="edit-rating">Edit Rating: </label><input type="text" id="edit-rating" name="edit-rating"><button id="submit-changes">Make Change</button></form>`)
+        console.log("The button has been clicked");
+    });
     //console.log($('#edit-movie option:selected').val());
 
-    $('#edit-movie').change(function(e){
-            console.log(e.target.value);
-    });
+    // $('#edit-movie').change(function(e){
+    //         console.log(e.target.value);
+    // });
 
 // button functionality to add a movie
     $('button').click(function() {
@@ -41,13 +48,16 @@ $(document).ready(function() {
         $('p').append({title: $('#movie-title').val(), rating: $('#movie-rating').val()});
           getMovies().then((movies) => {
               $('h1').html('Here are all the movies:');
-                  $('.movies').append(`<div class="movieObject"> ${movies[movies.length-1].title} - rating: ${movies[movies.length-1].rating}</div>`);
+                  $('.movies').append(`<div class="movieObject"> ${movies[movies.length-1].title} - rating: ${movies[movies.length-1].rating}
+                    </div><div class="edit-fields"><button class="delete">DELETE</button><button class="edit-button">EDIT</button></div>`);
                   $('#edit-movie').append(`<option>${movies[movies.length-1].title} - rating: ${movies[movies.length-1].rating}</option>`);
           }).catch((error) => {
               alert('Oh no! Something went wrong.\nCheck the console for details.');
               console.log(error);
           });
     });
+
+
+
 });
 
-// edit movie functionality coming soon!
