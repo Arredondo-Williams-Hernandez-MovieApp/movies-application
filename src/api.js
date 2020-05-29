c1cd297b8b01e994538d9a401bb8f8792b3d5591
Jay Arredondo <jearredondo13@gmail.com>
2020-05-29 12:31:41 -0500

    added and deleted comments and cleaned up syntax

--- src/api.js
+++ src/api.js
@@ -19,14 +19,4 @@ module.exports = {
      return fetch(url, options)
         .then(response => response.json());
   }
-  // addMovie: () => {
-  //   return fetch('/api/movies', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({title: 'Steal This Movie', rating: '1'})
-  //   })
-//         .then(response => response.json()).then(data => console.log('success', data));
-//   }
 };
--- src/index.js
+++ src/index.js
@@ -15,14 +15,10 @@ const {getMovies, addMovie} = require('./api.js');
 
 //First requirement
 $(document).ready(function() {
-  //psuedo
-  //while text input val === 0, run 'loading...'
-  //on input populate html, using live search, keyup event
-//
    getMovies().then((movies) => {
     var movieList = '';
     $('h1').html('Here are all the movies:');
-    movies.forEach(({title, rating,}) => {
+    movies.forEach(({title, rating}) => {
       movieList += $('.movies').append(`<div class="movieObject">${title} - rating: ${rating}</div>`);
         $('#edit-movie').append(`<option>${title} - rating: ${rating}</option>`)});
   }).catch((error) => {
@@ -31,20 +27,20 @@ $(document).ready(function() {
   });
 
 
-
+// button functionality to add a movie
     $('button').click(function() {
       $('h1').html('Loading...');
-      //let movieList = '';
           addMovie();
-        $('p').append({title: $('#movie-title').val(), rating: $('#movie-rating').val()})
+        $('p').append({title: $('#movie-title').val(), rating: $('#movie-rating').val()});
           getMovies().then((movies) => {
               $('h1').html('Here are all the movies:');
                   $('.movies').append(`<div class="movieObject"> ${movies[movies.length-1].title} - rating: ${movies[movies.length-1].rating}</div>`);
                   $('#edit-movie').append(`<option>${movies[movies.length-1].title} - rating: ${movies[movies.length-1].rating}</option>`);
-
           }).catch((error) => {
               alert('Oh no! Something went wrong.\nCheck the console for details.');
               console.log(error);
           });
     });
 });
+
+// edit movie functionality coming soon!
