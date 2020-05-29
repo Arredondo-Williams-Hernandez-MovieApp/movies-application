5ba661a3f3074a0b12f91f87ce419b8ae7034385
Jay Arredondo <jearredondo13@gmail.com>
2020-05-29 12:27:33 -0500

    added select menu for edit functionality and deleted the id text

--- src/index.js
+++ src/index.js
@@ -22,9 +22,9 @@ $(document).ready(function() {
    getMovies().then((movies) => {
     var movieList = '';
     $('h1').html('Here are all the movies:');
-    movies.forEach(({title, rating, id}) => {
-      movieList += $('.container').append(`id#${id} - ${title} - rating: ${rating}`);
-    });
+    movies.forEach(({title, rating,}) => {
+      movieList += $('.movies').append(`<div class="movieObject">${title} - rating: ${rating}</div>`);
+        $('#edit-movie').append(`<option>${title} - rating: ${rating}</option>`)});
   }).catch((error) => {
     alert('Oh no! Something went wrong.\nCheck the console for details.');
     console.log(error);
@@ -32,41 +32,19 @@ $(document).ready(function() {
 
 
 
-$('button').click(function() {
-  $('h1').html('Loading...');
-  //let movieList = '';
-      addMovie();
-    $('p').append({title: $('#movie-title').val(), rating: $('#movie-rating').val()})
-      getMovies().then((movies) => {
-          $('h1').html('Here are all the movies:');
-              $('.container').append(` id#${movies[movies.length-1].id} - ${movies[movies.length-1].title} - rating: ${movies[movies.length-1].rating}`);
-      }).catch((error) => {
-          alert('Oh no! Something went wrong.\nCheck the console for details.');
-          console.log(error);
-      });
-});
+    $('button').click(function() {
+      $('h1').html('Loading...');
+      //let movieList = '';
+          addMovie();
+        $('p').append({title: $('#movie-title').val(), rating: $('#movie-rating').val()})
+          getMovies().then((movies) => {
+              $('h1').html('Here are all the movies:');
+                  $('.movies').append(`<div class="movieObject"> ${movies[movies.length-1].title} - rating: ${movies[movies.length-1].rating}</div>`);
+                  $('#edit-movie').append(`<option>${movies[movies.length-1].title} - rating: ${movies[movies.length-1].rating}</option>`);
 
-  // $.ajax('/api/movies').done(function (data) {
-  //   $('#loading').text(`Here are all the movies:`);
-  //   data.forEach(({title, rating}) => {
-  //     $('.container').append(`<div class="movies">${title} - rating: ${rating}</div>`)
-  //   });
-  //
-  // $('button').click(function (e) {
-  //   $('h1').html('Loading...');
-  //   e.preventDefault();
-  //   $('.container').append(`<div class="movies">${$('#movie-title').val()} - rating: ${$('#movie-rating').val()} </div>`)
-  //   setTimeout(function () {
-  //     $('h1').html('Here are your movies ')
-  //   },2000);
-  //   $.post('/api/movies', {
-  //     "title": $('#movie-title').val(),
-  //     "rating": $('#movie-rating').val()
-  //   });
-  // });
-//   })
-//   })
-// });
-
-//
+          }).catch((error) => {
+              alert('Oh no! Something went wrong.\nCheck the console for details.');
+              console.log(error);
+          });
+    });
 });
